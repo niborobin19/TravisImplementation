f0b76b84618f8cef5e85c0e676f6c24117a023a0
niborobin19 <56249261+niborobin19@users.noreply.github.com>
2021-07-05 16:10:29 +0200

    changed download path

--- travis-build/install-unity.sh
+++ travis-build/install-unity.sh
@@ -3,8 +3,8 @@
 # See https://unity3d.com/get-unity/download/archive
 # to get download URLs
 UNITY_DOWNLOAD_CACHE="$(pwd)/unity_download_cache"
-UNITY_OSX_PACKAGE_URL="https://download.unity3d.com/download_unity/46dda1414e51/MacEditorInstaller/Unity-2020.3.12f1.pkg"
-UNITY_WINDOWS_TARGET_PACKAGE_URL="https://beta.unity3d.com/download/46dda1414e51/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2020.3.12f1.pkg"
+UNITY_OSX_PACKAGE_URL="https://download.unity3d.com/download_unity/b3b2c6512326/MacEditorTargetInstaller/UnitySetup-Mac-IL2CPP-Support-for-Editor-2020.3.12f1.pkg"
+UNITY_WINDOWS_TARGET_PACKAGE_URL="https://download.unity3d.com/download_unity/b3b2c6512326/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-2020.3.12f1.pkg"
 
 
 # Downloads a file if it does not exist
