ea16c6b1282ff40951e071a3ef356dfdb5cdbf21
niborobin19 <56249261+niborobin19@users.noreply.github.com>
2021-07-05 15:16:11 +0200

    removed ignored .csproj

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,51 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace BuildManager
+{
+    public class BuildManager : MonoBehaviour
+    {
+        #if UNITY_EDITOR
+
+        [MenuItem("Builds/Build My Client And Server")]
+        public static void MyBuild()
+        {
+            try
+            {
+                //BuildServer();
+                BuildClient();
+
+                Debug.Log($"<color=orange>Your Builds Client and Server are Ready ! </color>");
+            }
+            catch (System.Exception)
+            {
+                Debug.Log($"Build Failed !!!");
+                throw;
+            }
+        }
+
+        private static void BuildClient()
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = new[] { "Assets/Scenes/Travis.unity" };
+            buildPlayerOptions.locationPathName = "Builds/Travis.exe";
+            buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+            buildPlayerOptions.options = BuildOptions.None;
+
+            BuildPipeline.BuildPlayer(buildPlayerOptions);
+        }
+
+        private static void BuildServer()
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = new[] { "Assets/Scenes/Server.unity" };
+            buildPlayerOptions.locationPathName = "Builds/Server/Server.exe";
+            buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+            buildPlayerOptions.options = BuildOptions.EnableHeadlessMode;
+
+            BuildPipeline.BuildPlayer(buildPlayerOptions);
+        }
+
+        #endif  
+    }
+}
---
+++ Assets/Scripts/Builder.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Builder
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
