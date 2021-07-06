6d42364a7d94926fe5b3819151e2eb3dfd7efbf4
niborobin19 <56249261+niborobin19@users.noreply.github.com>
2021-07-06 10:27:52 +0200

    Removed Unit test

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -6,22 +6,22 @@
 #  -projectPath argument to point to the right location.
 
 ## Run the editor unit tests
-echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-runEditorTests \
-	-editorTestsResultFile $(pwd)/test.xml \
-	-quit
+# echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+# 	-batchmode \
+# 	-nographics \
+# 	-silent-crashes \
+# 	-logFile $(pwd)/unity.log \
+# 	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
+# 	-runEditorTests \
+# 	-editorTestsResultFile $(pwd)/test.xml \
+# 	-quit
 
-rc0=$?
-echo "Unit test logs"
-cat $(pwd)/test.xml
-# exit if tests failed
-if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
+# rc0=$?
+# echo "Unit test logs"
+# cat $(pwd)/test.xml
+# # exit if tests failed
+# if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
 
 ## Make the builds
 # Recall from install.sh that a separate module was needed for Windows build support
